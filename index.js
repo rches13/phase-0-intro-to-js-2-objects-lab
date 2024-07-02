1e944129c0dc915d9c7d5598a7cf213a29e158ba
Rodgers Chesoni <rodgerschesoni@gmail.com>
2024-07-02 07:40:53 +0300

    add objects lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+    name:"Sam",
+    streetAddress:"12 Broadway",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = {...employee}
+    newObj['streetAddress']= "11 Broadway"
+    return newObj
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+  const streetAddress = 'delete employee.name'
+  employee['streetAddress']= '12 Broadway'
+  return employee
+}
+function deleteFromEmployeeByKey(employee, key){
+  const newObj1 = {newObj1}
+  return newObj1
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+  delete employee.name;
+  return employee;
+}
